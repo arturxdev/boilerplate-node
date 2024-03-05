1b83654aa7516d8172a99e4ffe2650ae07525fe5
Arturo GUerrero <arturo.guerrero@koibanx.com>
2024-03-04 19:34:14 -0600

    feat: queryes

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import express from 'express'
+import listEndpoints from 'express-list-endpoints'
+import pino from 'pino'
+import { PrismaClient } from "@prisma/client";
+
+const app = express()
+const port = 3000
+const logger = pino()
+
+logger.info('Hello, World!')
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/campaign/award', async (req, res) => {
+  const params = req.query
+  const prisma = new PrismaClient();
+  const result = await prisma.awardCampaign.findMany({
+    include: {
+      award: true,
+    },
+    where: { campaignId: parseInt(params.campaignId as string) }
+  })
+  res.json(result)
+  return
+})
+app.get('/company/award', async (req, res) => {
+  const params = req.query
+  const prisma = new PrismaClient();
+  const result = await prisma.campaign.findMany({
+    include: {
+      awardCampaign: { include: { award: true } }
+    },
+    where: { companyId: parseInt(params.companyId as string) }
+  })
+  res.json(result)
+  return
+})
+
+for (const endpoint of listEndpoints(app)) {
+  logger.info(`${endpoint.methods} ${endpoint.path}`)
+}
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
